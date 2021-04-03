8876d64dbf775380e0b39f84ee228b4a5f62110e
Jose Orozco <jose816orozco@gmail.com>
2021-04-02 23:50:33 -0500

    creating variables for password

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,10 @@
-var uppercase = ["A", "B", "C", "D", "E", "F"];
+var uppercase = ["A", "B", "C", "D", "E", "F",'G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',];
+
+var lowercase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','o','p','q','r','s','t','u','v','w','x','y','z']
+
+var numbers = ['1','2','3','4','5','6','7','8','9','0']
+
+var special = ['!','@','#','$','%','^','&','*']
 
 function getPasswordOpts() {
   var length = parseInt(prompt("How many characters would you like?"));
@@ -32,9 +38,15 @@ function getPasswordOpts() {
   return passwordOptions;
 }
 function generatePassword() {
-  var options = getPasswordOpts();
-  console.log(options);
+let complexity = document.getElementById(passwordOptions).value;
+let password = "";
+for (let i = 0; i <= passwordOptions.length; i++) 
+  password = password + value.charAt(Math.floor(Math.random() * Math.floor;(value.length -1)));
+  document.getElementsByid(passwordOptions).value = password
 }
+
+
+
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
